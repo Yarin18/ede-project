fc43d55e3f0d986cb4a464f2dd7a3b3904325230
Yarin van Raam <r0878916@student.thomasmore.be>
2023-12-27 11:55:27 +0100

    trigger workflow

--- workout-plan-service/src/main/java/fact/it/WorkoutPlanServiceApplication.java
+++ workout-plan-service/src/main/java/fact/it/WorkoutPlanServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WorkoutPlanServiceApplication {
 
+    // trigger workflow push lol
     public static void main(String[] args) {
         SpringApplication.run(WorkoutPlanServiceApplication.class, args);
     }
