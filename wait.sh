c4a09fa8c7d8a562a1e48170f8f6772128f74c7f
Yarin van Raam <r0878916@student.thomasmore.be>
2023-12-28 13:18:43 +0100

    remove wait

--- wait.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-until mysqladmin ping -h"$mysql-nutrition" --silent; do
- echo "$(date) - waiting for database to start"
- sleep 2
-done
-echo "$(date) - connected successfully"
-exec $@
