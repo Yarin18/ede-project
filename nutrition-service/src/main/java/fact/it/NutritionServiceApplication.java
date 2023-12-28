92d8c589225ac904728232b3362c8a6763196751
Yarin van Raam <r0878916@student.thomasmore.be>
2023-12-28 12:13:52 +0100

    attempt number 1000000

--- nutrition-service/src/main/java/fact/it/NutritionServiceApplication.java
+++ nutrition-service/src/main/java/fact/it/NutritionServiceApplication.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
@@ -15,24 +18,19 @@ public class NutritionServiceApplication  {
 
     private static final Logger logger = LoggerFactory.getLogger(NutritionServiceApplication.class);
 
-    @Autowired
-    private Environment environment;
-
-    @Value("${spring.datasource.url}")
-    private String test;
-    @PostConstruct
-    public void init() {
-        for (int i = 0; i < 50; i++) {
-            logger.info("????");
-            System.out.print("AYOOO " + test);
-            System.out.print("k");
-        }
-        for (int i = 0; i < 10; i++) {
-            logger.info("HOST " + environment.getProperty("MYSQL_DB_HOST"));
-        }
-    }
-
     public static void main(String[] args) {
-        SpringApplication.run(NutritionServiceApplication.class, args);
+        SpringApplication application = new SpringApplication(NutritionServiceApplication.class);
+        application.addInitializers(new CustomContextInitializer());
+        application.run(args);
     }
+
+    static class CustomContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            ConfigurableEnvironment environment = applicationContext.getEnvironment();
+            String myProperty = environment.getProperty("MYSQL_DB_HOST");
+            logger.info("PROPERTY " + myProperty);
+        }
+    }
+
 }
