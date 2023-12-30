59643d8ca3b91776095c21763622f1e5bae1cd8f
Yarin van Raam <r0878916@student.thomasmore.be>
2023-12-30 11:39:26 +0100

    disable activemq

--- user-service/src/main/java/fact/it/mq/MessageSender.java
+++ user-service/src/main/java/fact/it/mq/MessageSender.java
@@ -18,13 +18,13 @@ public class MessageSender {
     private ObjectMapper objectMapper;
 
     public <T extends Nameable> void sendMessage(final T msg, final UserController.UserTopic topic) {
-        try {
-            String jsonString = objectMapper.writeValueAsString(msg);
-            jmsMessagingTemplate.convertAndSend(topic.getName(), jsonString);
-            System.out.println("Sent message!");
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            String jsonString = objectMapper.writeValueAsString(msg);
+//            jmsMessagingTemplate.convertAndSend(topic.getName(), jsonString);
+//            System.out.println("Sent message!");
+//        } catch (JsonProcessingException e) {
+//            e.printStackTrace();
+//        }
     }
 
 }
