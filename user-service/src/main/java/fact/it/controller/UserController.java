2792b11818e17f6750129c621690fdcb988f0c67
Yarin van Raam <r0878916@student.thomasmore.be>
2023-12-30 11:52:32 +0100

    idkf

--- user-service/src/main/java/fact/it/controller/UserController.java
+++ user-service/src/main/java/fact/it/controller/UserController.java
@@ -5,7 +5,6 @@ import fact.it.dto.UserResponse;
 import fact.it.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,9 +13,6 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @Slf4j
 public class UserController {
-
-//    @Autowired
-//    private MessageSender messageSender;
 
     private final UserService userService;
 
@@ -29,39 +25,19 @@ public class UserController {
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
     public void createUser(final @RequestBody UserRequest userRequest) {
-//        messageSender.sendMessage(userRequest, UserTopic.CREATE);
         userService.createUser(userRequest);
     }
 
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteUser(final @PathVariable("id") String id) {
-//        messageSender.sendMessage(userService.getById(id), UserTopic.DELETE);
         userService.deleteUser(id);
     }
 
     @PutMapping("{id}")
     @ResponseStatus(HttpStatus.OK)
     public void updateUser(final @PathVariable("id") String id, final @RequestBody UserRequest userRequest) {
-//        messageSender.sendMessage(userRequest, UserTopic.UPDATE);
         userService.updateUser(id, userRequest);
-    }
-
-    public enum UserTopic {
-
-        CREATE("create-user"),
-        DELETE("delete-user"),
-        UPDATE("update-user");
-
-        private final String name;
-
-        UserTopic(final String name) {
-            this.name = name;
-        }
-
-        public String getName() {
-            return name;
-        }
     }
 
 }
