e9846845c2487f71bada06b36f71ac35fb0e6055
Yarin van Raam <r0878916@student.thomasmore.be>
2024-01-01 16:07:34 +0100

    latest

---
+++ api-gateway-service/src/main/java/fact/it/security/Config.java
@@ -0,0 +1,32 @@
+package fact.it.security;
+
+import io.netty.handler.codec.http.HttpMethod;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebFluxSecurity
+public class Config {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity
+                .authorizeExchange(exchange ->
+                        exchange
+                                .pathMatchers("/workout", "/nutrition", "/user")
+                                .authenticated()
+                                .anyExchange()
+                                .authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(withDefaults())
+                );
+        return serverHttpSecurity.build();
+    }
+
+}
