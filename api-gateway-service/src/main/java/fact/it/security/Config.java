969cbd4ab58af2ef154d5c1086f03044359f3d4c
Yarin van Raam <r0878916@student.thomasmore.be>
2024-01-04 11:03:44 +0100

    test rate limiter + remove depdencey dupe

--- api-gateway-service/src/main/java/fact/it/security/Config.java
+++ api-gateway-service/src/main/java/fact/it/security/Config.java
@@ -9,6 +9,7 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnection;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -22,6 +23,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebFluxSecurity
+@EnableRedisRepositories
 public class Config {
 
     @Bean
@@ -38,6 +40,27 @@ public class Config {
                         .jwt(withDefaults())
                 );
         return serverHttpSecurity.build();
+    }
+
+    /** The redis configuration .*/
+    @Bean
+    public RedisStandaloneConfiguration redisConfig() {
+        return new RedisStandaloneConfiguration("redis", 6379);
+    }
+
+    /** The method to create a LettuceConnectionFactory based of the redisConfig. */
+    @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        return new LettuceConnectionFactory(redisConfig());
+    }
+
+    /** Fully configured RedisTemplate.*/
+    @Bean
+    public RedisTemplate<String, Object> template() {
+        final RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(lettuceConnectionFactory());
+        redisTemplate.setKeySerializer(RedisSerializer.string());
+        return redisTemplate;
     }
 
     @Bean
